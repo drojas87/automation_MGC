5eaf2eaff555067a9a649d29624856bc49cc5fd2
DIEGO ROJAS <drojasrojasd@gmail.com>
2021-01-12 16:18:16 -0500

    Initial folder structure

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+
+# gem "rails"
+
+# Added at 2021-01-12 16:01:36 -0500 by diego:
+gem "cucumber", "~> 5.2"
---
+++ features/step_definitions/google_homepage_search.rb
@@ -0,0 +1,39 @@
+Given('I’m on the homepage') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I type “The name of the wind” into the search field') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I click the Google Search button') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('I go to the search results page') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('the first result is “The Name of the Wind - Patrick Rothfuss”') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I click on the first result link') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then('I go to the “Patrick Rothfuss - The Books” page') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I type “The name of the w” into the search field') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('the suggestions list is displayed') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  When('I click on the first suggestion in the list') do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
